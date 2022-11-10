d0cd003692e04ec278e4adb0e00a6e4a0c3442c1
MOHAMEDAMINE-ship-it <73406645+MOHAMEDAMINE-ship-it@users.noreply.github.com>
2022-11-10 18:40:30 +0100

    commm

---
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
@@ -0,0 +1,25 @@
+package tn.esprit.rh.achat;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import tn.esprit.rh.achat.entities.CategorieProduit;
+import tn.esprit.rh.achat.repositories.CategorieProduitRepository;
+import tn.esprit.rh.achat.services.CategorieProduitServiceImpl;
+
+@SpringBootTest
+
+public class AchatApplicationTests {
+	@Test
+    void contextLoads() {
+    }
+	
+}
